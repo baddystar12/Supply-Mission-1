e4ef04ada502af2a4ebaf35d602dfd38bf8410b6
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-10-20 19:37:25 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup() {
 	rectMode(CENTER);
 	
 
-	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite=createSprite(width/2, 80, 10,10, package_options);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
 
@@ -24,7 +24,7 @@ function setup() {
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
 
-	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite=createSprite(width/2, height-35, width,10, ground_options);
 	groundSprite.shapeColor=color(255)
 
 
@@ -47,19 +47,20 @@ function setup() {
   var package_options={
 	  restitution: 1
   }
+
 }
 
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-  drawSprites();
+	background("turquoise");
+	Engine.update(engine);
+	rectMode(CENTER);
+    rect(ground.position.x, ground.position.y, 400, 20);
+    ellipse(package.position.x, package.position.y, 20, 20);
+    drawSprites();
+ 
 }
+
 if(keyDown("space")){
 	restitution: 1
 }
-
-
-
